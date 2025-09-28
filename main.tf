716d281956f09ec26654167d111d5e92c40c7f7d
K S Shekhar <8143220231k@gmail.com>
2025-09-28 22:33:16 +0530

    refactor: Correctly implement private endpoints

--- main.tf
+++ main.tf
@@ -3,13 +3,11 @@ data "azurerm_resource_group" "rg" {
 }
 
 module "networking" {
-  source                = "./modules/networking"
-  prefix                = var.prefix
-  location              = data.azurerm_resource_group.rg.location
-  resource_group_name   = data.azurerm_resource_group.rg.name
-  tags                  = local.common_tags
-  sql_server_id         = module.database.sql_server_id
-  cosmosdb_account_id   = module.database.cosmosdb_account_id
+  source              = "./modules/networking"
+  prefix              = var.prefix
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  tags                = local.common_tags
 }
 
 module "security" {
@@ -58,4 +56,72 @@ module "backup" {
   resource_group_name = data.azurerm_resource_group.rg.name
   vm_ids              = module.compute.vm_ids
   tags                = local.common_tags
+}
+
+# --- Private Endpoint for Azure SQL ---
+resource "azurerm_private_dns_zone" "sql" {
+  name                = "privatelink.database.windows.net"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  tags                = local.common_tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "sql" {
+  name                  = "${var.prefix}-sql-dns-link"
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.sql.name
+  virtual_network_id    = module.networking.vnet_id # Assuming vnet_id is an output from networking module
+}
+
+resource "azurerm_private_endpoint" "sql" {
+  name                = "${var.prefix}-sql-pe"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  subnet_id           = module.networking.subnet_id
+  tags                = local.common_tags
+
+  private_service_connection {
+    name                           = "${var.prefix}-sql-psc"
+    private_connection_resource_id = module.database.sql_server_id
+    is_manual_connection           = false
+    subresource_names              = ["sqlServer"]
+  }
+
+  private_dns_zone_group {
+    name                 = "default"
+    private_dns_zone_ids = [azurerm_private_dns_zone.sql.id]
+  }
+}
+
+# --- Private Endpoint for Cosmos DB ---
+resource "azurerm_private_dns_zone" "cosmos" {
+  name                = "privatelink.documents.azure.com"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  tags                = local.common_tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "cosmos" {
+  name                  = "${var.prefix}-cosmos-dns-link"
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.cosmos.name
+  virtual_network_id    = module.networking.vnet_id # Assuming vnet_id is an output from networking module
+}
+
+resource "azurerm_private_endpoint" "cosmos" {
+  name                = "${var.prefix}-cosmos-pe"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  subnet_id           = module.networking.subnet_id
+  tags                = local.common_tags
+
+  private_service_connection {
+    name                           = "${var.prefix}-cosmos-psc"
+    private_connection_resource_id = module.database.cosmosdb_account_id
+    is_manual_connection           = false
+    subresource_names              = ["Sql"]
+  }
+
+  private_dns_zone_group {
+    name                 = "default"
+    private_dns_zone_ids = [azurerm_private_dns_zone.cosmos.id]
+  }
 }
--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -79,85 +79,14 @@ resource "azurerm_lb_probe" "https8443_probe" {
   port                = 8443
 }
 
-# --- Private Endpoint for Azure SQL ---
-resource "azurerm_private_dns_zone" "sql" {
-  count               = var.sql_server_id != null ? 1 : 0
-  name                = "privatelink.database.windows.net"
-  resource_group_name = var.resource_group_name
-  tags                = var.tags
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "sql" {
-  count                 = var.sql_server_id != null ? 1 : 0
-  name                  = "${var.prefix}-sql-dns-link"
-  resource_group_name   = var.resource_group_name
-  private_dns_zone_name = azurerm_private_dns_zone.sql[0].name
-  virtual_network_id    = azurerm_virtual_network.vnet.id
-}
-
-resource "azurerm_private_endpoint" "sql" {
-  count               = var.sql_server_id != null ? 1 : 0
-  name                = "${var.prefix}-sql-pe"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  subnet_id           = azurerm_subnet.subnet.id
-  tags                = var.tags
-
-  private_service_connection {
-    name                           = "${var.prefix}-sql-psc"
-    private_connection_resource_id = var.sql_server_id
-    is_manual_connection           = false
-    subresource_names              = ["sqlServer"]
-  }
-
-  private_dns_zone_group {
-    name                 = "default"
-    private_dns_zone_ids = [azurerm_private_dns_zone.sql[0].id]
-  }
-}
-
-# --- Private Endpoint for Cosmos DB ---
-resource "azurerm_private_dns_zone" "cosmos" {
-  count               = var.cosmosdb_account_id != null ? 1 : 0
-  name                = "privatelink.documents.azure.com"
-  resource_group_name = var.resource_group_name
-  tags                = var.tags
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "cosmos" {
-  count                 = var.cosmosdb_account_id != null ? 1 : 0
-  name                  = "${var.prefix}-cosmos-dns-link"
-  resource_group_name   = var.resource_group_name
-  private_dns_zone_name = azurerm_private_dns_zone.cosmos[0].name
-  virtual_network_id    = azurerm_virtual_network.vnet.id
-}
-
-resource "azurerm_private_endpoint" "cosmos" {
-  count               = var.cosmosdb_account_id != null ? 1 : 0
-  name                = "${var.prefix}-cosmos-pe"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  subnet_id           = azurerm_subnet.subnet.id
-  tags                = var.tags
-
-  private_service_connection {
-    name                           = "${var.prefix}-cosmos-psc"
-    private_connection_resource_id = var.cosmosdb_account_id
-    is_manual_connection           = false
-    subresource_names              = ["Sql"]
-  }
-
-  private_dns_zone_group {
-    name                 = "default"
-    private_dns_zone_ids = [azurerm_private_dns_zone.cosmos[0].id]
-  }
-}
-
 output "subnet_id" {
   value = azurerm_subnet.subnet.id
 }
 
-
+output "vnet_id" {
+  description = "The ID of the virtual network."
+  value       = azurerm_virtual_network.vnet.id
+}
 
 output "lb_nat_rule_ids" {
   value = azurerm_lb_nat_rule.ssh_nat_rule.*.id
